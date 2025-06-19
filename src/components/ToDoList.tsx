5682e8a5131f4180c244003bb0ad8d8c1fd6e3eb
Sajeed <syedsajeed6969@gmail.com>
2025-06-20 00:12:16 +0530

    commit

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,42 +1,51 @@
-import { Table } from "antd";
+import { Button, Space, Table } from "antd";
 import React from "react";
 const ToDoList: React.FC = () => {
-    const dataSource = [
-  {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
-  },
-  {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
-  },
-];
+  const dataSource = [
+    {
+      id: 1,
+      title: "Mike",
+      description: "32",
+      address: "10 Downing Street",
+    },
+    {
+      id: 2,
+      title: "John",
+      description: "42",
+      address: "10 Downing Street",
+    },
+  ];
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
-  return <div>
-    <h3>My To Dos</h3>
-    <Table dataSource={dataSource} columns={columns} />;
-
-  </div>;
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Action",
+      key: "action",
+      width:20,
+      render: (_: any, record: any) => (
+        <Space size="middle">
+          <Button>Invite {record.title}</Button>
+          <Button>Delete</Button>
+        </Space>
+      ),
+    },
+  ];
+  return (
+    <div style={{ margin: 5 }}>
+      <h3>My To Dos</h3>
+      <div style={{ padding: 5 }}>
+        <Table dataSource={dataSource} columns={columns} />
+      </div>
+    </div>
+  );
 };
 export default ToDoList;
